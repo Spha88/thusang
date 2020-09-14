aeadcf775a620f27dd2e0edf0e8da1b3166d1338
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-09-14 06:14:49 +0200

    Add about us page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import Decal from '../../UI/Decal/Decal'
+import classes from './About.module.scss';
+import React from 'react';
+import Decal from '../../UI/Decal/Decal';
+import Container from '../../UI/Container/Container';
 
 const About = () => {
     return (
-        <div>
+        <div className={classes.AboutUs}>
             <Decal heading="About Us" />
+
+            <Container>
+                <div className={classes.Content}>
+                    <p><span>Thusang Social Center</span> is committed to reachout to the world on behalf of those without resources and means to do so.</p>
+
+                    <p>We do so with deep confidence and belief that there is someone out there willing to extend a helping to women and children in distress, who are daily subjected to an abusive and sub-human behaviour of men who deserve no part in a humanitarian society. We rely on your kind-heartedness to make this reality a dream come true for the victims affected by GBV, rape and femicide.</p>
+
+                    <h2>Our <span>Mission</span></h2>
+
+                    <p>Our mission is to provide, safe havens and shelter for women and children, access to immediate professional counselling services and support.</p>
+
+                    <p>We do that, by empowering victims with life skills and recovery programs and providing necessary resources in underprivileged areas and families. We believe in smart fund allocation, and therefore we employ minimal staff.</p>
+
+                    <h2>Our <span>Beliefs</span></h2>
+
+                    <p>We believe in a world where every mother and child are safe.</p>
+                </div>
+            </Container>
         </div>
     )
 }
