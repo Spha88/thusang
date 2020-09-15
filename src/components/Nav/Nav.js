5ac38b90d9c81cbc0dfee37d52e92e1dfe1988af
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-09-15 06:27:27 +0200

    Add mobile nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Nav.module.scss';
 import { NavLink } from 'react-router-dom';
+import burger_icon from '../../assets/images/burger_icon.png';
 
 const Nav = () => {
+    const [openNav, setOpenNav] = useState(false);
     return (
         <header className={classes.NavContainer}>
             <nav className={classes.Nav}>
@@ -10,7 +12,16 @@ const Nav = () => {
                     <h2>Thusang Social Center</h2>
                     <h5>In the service of caring</h5>
                 </div>
+                <div className={classes.NavIconContainer} onClick={() => setOpenNav(!openNav)}>
+                    <img src={burger_icon} alt="nav icon" />
+                </div>
                 <ul className={classes.NavItems}>
+                    <li><NavLink activeClassName={classes.Active} exact to="/">Home</NavLink></li>
+                    <li><NavLink activeClassName={classes.Active} to="/about">About Us</NavLink></li>
+                    <li><NavLink activeClassName={classes.Active} to="/donate">Donate</NavLink></li>
+                    <li><NavLink activeClassName={classes.Active} to="/contact-us">Contact</NavLink></li>
+                </ul>
+                <ul className={`${classes.NavItemsMobile} ${openNav && classes.open}`}>
                     <li><NavLink activeClassName={classes.Active} exact to="/">Home</NavLink></li>
                     <li><NavLink activeClassName={classes.Active} to="/about">About Us</NavLink></li>
                     <li><NavLink activeClassName={classes.Active} to="/donate">Donate</NavLink></li>
