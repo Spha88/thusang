be684cb0147b9d68b63b7daec622dee6a69bacd9
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-09-27 09:00:44 +0200

    Add animations to donate page

--- src/components/pages/Donate/Donate.js
+++ src/components/pages/Donate/Donate.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import Zoom from 'react-reveal/Zoom';
 import classes from './Donate.module.scss';
 import Decal from '../../UI/Decal/Decal';
 import Container from '../../UI/Container/Container';
@@ -13,18 +14,22 @@ const Donate = () => {
             <Container>
                 <div className={classes.Content}>
                     <div className={classes.BankingDetails}>
-                        <h2><span>Donations</span> are welcome at:</h2>
-                        <ul>
-                            <li><span>Bank</span>: First National Bank</li>
-                            <li><span>Account name</span>: Thusang Social Center</li>
-                            <li><span>Account number</span>: <span className={classes.Numbers}>6285 7532 991</span></li>
-                            <li><span>Account type</span>: Cheque</li>
-                            <li><span>Branch code</span>: <span className={classes.Numbers}>250655</span> </li>
-                        </ul>
-                        <h3>"No amount is too little"</h3>
+                        <h2><Zoom><span>Donations</span></Zoom> <Zoom delay={500}><div style={{ display: 'inline-block' }}>are welcome at:</div></Zoom></h2>
+                        <Zoom>
+                            <ul>
+                                <li><span>Bank</span>: First National Bank</li>
+                                <li><span>Account name</span>: Thusang Social Center</li>
+                                <li><span>Account number</span>: <span className={classes.Numbers}>6285 7532 991</span></li>
+                                <li><span>Account type</span>: Cheque</li>
+                                <li><span>Branch code</span>: <span className={classes.Numbers}>250655</span> </li>
+                            </ul>
+                        </Zoom>
+                        <Zoom delay={1000}><h3>"No amount is too little"</h3></Zoom>
                     </div>
                     <div className={classes.Image}>
-                        <img src={thank_you_img} alt="thank graphic" />
+                        <Zoom>
+                            <img src={thank_you_img} alt="thank graphic" />
+                        </Zoom>
                     </div>
                 </div>
             </Container>
