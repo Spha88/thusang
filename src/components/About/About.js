d5fbeb695de1cac526612a203edef65e970093d7
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-09-27 11:21:02 +0200

    Complete CSSImageSlide Component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,7 @@ import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
 import classes from './About.module.scss';
 import Container from '../UI/Container/Container';
+import CssImageSlide from '../UI/CssImageSlide/CssImageSlide';
 
 const About = () => {
     return (
@@ -11,15 +12,15 @@ const About = () => {
             <section className={classes.About}>
                 <div className={classes.Content}>
                     <Fade delay={500}><h2><span>About</span> Us</h2></Fade>
-                    <Fade>
+                    <Zoom delay={1000}>
                         <p><span>Thusang Social Center</span> is non-profit organisation established to provide support services to individuals and families affected by gender-based violence and femicide.
                     We are based in Aliwal North, in the Eastern Cape province of South Africa.</p>
-                    </Fade>
+                    </Zoom>
                 </div>
 
                 <div className={classes.Image}>
-                    <Zoom delay={1000} fraction={0.9}>
-                        <img src={aboutImage} alt="dipiction of violence" />
+                    <Zoom>
+                        <CssImageSlide />
                     </Zoom>
                 </div>
 
