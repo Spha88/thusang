14c4b5549f1f57a753bcf8a9ab80cf6b03069a60
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-09-29 06:12:26 +0200

    Customise page scroll bar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'react-app-polyfill/ie9';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(
